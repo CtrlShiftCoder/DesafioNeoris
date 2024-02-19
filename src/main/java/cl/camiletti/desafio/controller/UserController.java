93ea6922ed36d89dcd01a48dfbcb7cee00b3cbdb
CtrlShiftCoder <ignacioandrecamilettim@gmail.com>
2024-02-18 21:58:49 -0300

    Corrección

--- src/main/java/cl/camiletti/desafio/controller/UserController.java
+++ src/main/java/cl/camiletti/desafio/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 @Tag(name = "Users")
 public class UserController {
